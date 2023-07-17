833b95c652c0d1e43550a8ab6270712909d0eb72
HarishKathir <harishbabu1532002@gmail.com>
2023-07-17 12:26:03 +0530

    Initial Commit

---
+++ FarmOraganicsApp/FarmOraganicsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmOraganicsAppApp.swift
+//  FarmOraganicsApp
+//
+//  Created by Harishkathir on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FarmOraganicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
